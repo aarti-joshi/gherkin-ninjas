5033571a1e8c6a99cfbf1c073ee526a84a9c658e
khalifa <oriyomibamikole@gmail.com>
2023-10-10 20:43:54 +0100

    added functionality so bar loads to current point on page load

--- client/homepage/scripts.js
+++ client/homepage/scripts.js
@@ -11,17 +11,42 @@ progressBar.style.strokeDasharray = `${circumference} ${circumference}`;
 progressBar.style.strokeDashoffset = `${circumference}`;
 
 // Function to update progress
+// function updateProgress(points, nextGoal) {
+//   // Calculate progress as a percentage
+//   const progress = points / nextGoal;
+
+//   // Update stroke-dashoffset based on progress
+//   const offset = circumference - (progress * circumference);
+//   progressBar.style.strokeDashoffset = offset;
+
+//   // Update text
+//   progressText.textContent = `${points} / ${nextGoal}`;
+// }
+// Function to update progress with animation
 function updateProgress(points, nextGoal) {
-  // Calculate progress as a percentage
-  const progress = points / nextGoal;
-
-  // Update stroke-dashoffset based on progress
-  const offset = circumference - (progress * circumference);
-  progressBar.style.strokeDashoffset = offset;
-
-  // Update text
-  progressText.textContent = `${points} / ${nextGoal}`;
-}
+    // Calculate progress as a percentage
+    const progress = points / nextGoal;
+  
+    // Calculate the final stroke-dashoffset based on progress
+    const finalOffset = circumference - (progress * circumference);
+  
+    let currentOffset = circumference;
+    const step = (circumference - finalOffset) / 100;
+    const intervalTime = 10; // Time in milliseconds for each step
+    
+    const interval = setInterval(() => {
+      if (currentOffset <= finalOffset) {
+        clearInterval(interval);
+      } else {
+        currentOffset -= step;
+        progressBar.style.strokeDashoffset = currentOffset;
+      }
+    }, intervalTime);
+  
+    // Update text
+    progressText.textContent = `${points} / ${nextGoal}`;
+  }
+  
 
 // Test the function
 updateProgress(30, 100);
